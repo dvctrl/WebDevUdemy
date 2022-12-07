57564d6281ba05f16368a4cf4fcc7bdfaedeed26
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2022-12-07 12:42:53 -0300

    if_else2 atualizado

--- 2.Estruturas de controle/1_IF_js/if_else2.js
+++ 2.Estruturas de controle/1_IF_js/if_else2.js
@@ -1,34 +1,33 @@
-Number.prototype.entre = function(inicio, fim){
-        return this >= inicio && this <= fim 
+Number.prototype.entre = function (inicio, fim) {
+  return this >= inicio && this <= fim;
+};
 
-}
+const imprimirResultado = function (nota) {
+  if (nota.entre(9, 10)) {
+    console.log("Quadro de honra");
+  } else if (nota.entre(7, 8.99)) {
+    console.log("Aprovado");
+  } else if (nota.entre(4, 6.99)) {
+    console.log("Recuperação");
+  } else if (nota.entre(0, 3.99)) {
+    console.log("Tu é muito burro, reprovadissímo");
+  } else {
+    console.log("nota invalida");
+  }
+  console.log(".........");
+};
 
-const imprimirResultado = function(nota){
-    if (nota.entre(9,10)){
-        console.log('Quadro de honra')
-    }   else if(nota.entre(7,8.99)){
-            console.log('Aprovado')
-    }       else if(nota.entre(4,6.99)){
-                console.log('Recuperação')
-    }           else if(nota.entre(0,3.99)){
-                    console.log('Tu é muito burro, reprovadissímo')
-                } else{
-                    console.log('nota invalida')
-                }
-    console.log('.........')
-}
-
-imprimirResultado(10)
-imprimirResultado(4)
-imprimirResultado(6.99)
-imprimirResultado(7.25)
-imprimirResultado(9)
-imprimirResultado(-2)
-imprimirResultado(-1)
-imprimirResultado(12.99)
+imprimirResultado(10);
+imprimirResultado(4);
+imprimirResultado(6.99);
+imprimirResultado(7.25);
+imprimirResultado(9);
+imprimirResultado(-2);
+imprimirResultado(-1);
+imprimirResultado(12.99);
 
 /* Neste exemplo acima, nota-se que podemos usar o condicional
  * 'if' e 'else if' de forma encadeada gerando diversos testes
  * em nossa programação
- * 
+ *
  */
