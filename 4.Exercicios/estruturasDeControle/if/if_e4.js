a69fb9a25905d95006accca67aa4fb6bd119b0d9
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2022-12-10 11:20:00 -0300

    criação de pasta estruturas de controle

---
+++ 4.Exercicios/estruturasDeControle/if/if_e4.js
@@ -0,0 +1,11 @@
+var semaforo = "verde";
+
+if (semaforo == "verde") {
+  console.log("avançar");
+} else if (semaforo == "vermelho") {
+  console.log("pare");
+} else if (semaforo == "amarelo") {
+  console.log("reduza");
+} else {
+  console.log("valor invalido");
+}
