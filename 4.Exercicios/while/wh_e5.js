03199685a22692a1843863856af4ba8d12efac1a
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2022-12-10 10:33:17 -0300

    exercicio fixação while

---
+++ 4.Exercicios/while/wh_e5.js
@@ -0,0 +1,11 @@
+let carro = [
+    {modelo:'opala', valor:'20000', cor: 'preto'},
+    {modelo:'chevete', valor:'19000', cor: 'amarelo'}
+]
+
+let i = 0
+
+while (i < carro.length) {
+    console.log(i,carro[i])
+    i++
+}
